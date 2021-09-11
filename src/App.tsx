cdb6a48612bee68c56a3735e644cb767ba1c0964
Karan Tondare <karanstondare999@gmail.com>
2021-09-11 12:59:16 +0530

    - added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Header } from './components/header/header.component';
 import { Homepage } from './pages/homepage/homepage.component';
 import { ShopPage } from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.styles.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/contact'>
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
